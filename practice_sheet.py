79f1a31066c65a127f6944d7b4829fe47a9b1809
vs369-code <vibhutik665@gmail.com>
2026-09-05 04:57:58 +0530

    Add files via upload
    In this file i solve 19 out of 30 question with psudo code in first time (day-1)

---
+++ practice_sheet.py
@@ -0,0 +1,159 @@
+#1. usr input hello, name!
+print("Hello, name!")
+
+#2. difference b/w two number
+'''a = 20
+b = 10
+print(a - b)'''
+
+#3. sum b/w two number
+'''a = 20
+b = 10
+print(a+b)'''
+
+#4. product of two number
+'''a = 20
+b = 20
+print(a*b)'''
+
+#5. divide of number
+'''a = 20
+b = 10
+print(a/b)'''
+
+#6. square of given number
+'''a = 20
+print(a*a)'''
+
+#7. cube of given number
+a = 20
+print(a*a*a)
+
+#8. area of rectangle 
+l = 20
+b = 30
+area = l * b
+print(area)
+
+#9. area of circle 
+r = 20
+area = 3.14 * r * r
+print(area)
+
+#10. find simple interest of given number 
+p = 20000
+r = 2
+t = 1
+si = (p*r*t)/100
+print(si)
+
+#11. find even or odd number
+num = 25
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("odd number")
+
+#12. find poitive or negative number
+a = -10
+
+if a<0:
+    print("negative number")
+else:
+    print("positive number")   
+
+#13. write eligiblity criteria for vote
+age = 50
+
+if age >= 18:
+    print("Eligible for vote")
+else:
+    print("Not eligible for vote")    
+
+#14. find the largest number between two number 
+a = 10
+b = 10
+
+if a < b:
+    print("b is greater than a")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is greater than b")    
+
+#15. find largest number in three number
+a = 0
+b = 51
+c = 50
+
+if a > b > c: 
+    print("a is greater than all") 
+elif b > a > c:
+    print("b is greater than all")
+ 
+elif c > a > b:
+    print("c i greater than all ")
+else:
+    print("all number are equal") 
+
+#16. find smallest number in three number
+w = 20
+x = 40
+y = 10
+
+if x < y < w:
+    print("x is smallest than all three number")
+elif y < x < w:
+    print("y is smallest than all three number")
+elif w < y < x:
+    print("w is smallest than all three number") 
+else:
+    print("all number are equal") 
+
+#17. write program of pass or fail
+marks = 30
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")   
+
+#18. write a program of grade calculator
+m = 52
+if m >= 80:
+    print("A")
+elif m >= 70:
+    print("B")   
+elif m >= 60:
+    print("C") 
+elif m >= 50:
+    print("D") 
+elif m >= 40:
+    print("E")
+else:
+    print("fail")   
+
+#19. write a program which is divisible by 5
+num = 11
+if num % 5 == 0:
+    print("yes")
+else:
+    print("No")   
+
+#20. find leap year
+year = 2024
+if year % 400 == 0 or year % 4==0 and year % 100 != 0:
+    print("leap year")
+else:
+    print("no it's not leap year") 
+
+ 
+ 
+
+ 
+
+          
+             
+
+
+    
